78c3c6a9b01cbbd458ba6f8f6ce41765e7c7cc70
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-09-03 23:53:07 +0530

    blood donation

--- blood donation.sql
+++ blood donation.sql
@@ -2,3 +2,13 @@ CREATE DATABASE Vaibhav1;
 
 USE Vaibhav1;
 
+CREATE TABLE project (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Mobile_number VARCHAR(15) NOT NULL,
+    City VARCHAR(100),
+    Address VARCHAR(255),
+    Passw VARCHAR(20) NOT NULL  -- Password stored as a string
+);
+
