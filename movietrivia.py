b0c2989ecb655bd4c48501d21abf9e512da1e604
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-09-19 20:44:46 +0530

    project

--- movietrivia.py
+++ movietrivia.py
@@ -4,3 +4,12 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
+app = Flask(__name__)
+
+# Helper function to scrape IMDb and return results
+def search_imdb(query):
+    search_url = f"https://www.imdb.com/find?q={query}&s=tt"
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    
