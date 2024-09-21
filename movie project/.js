ef9f26bdd1171399f282a8bde0d67b4d2e084c66
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-09-21 23:41:42 +0530

    project

--- movie project/.js
+++ movie project/.js
@@ -3,3 +3,8 @@ const apiKey = ' 9c8791f0'; // Replace with your API keyp
 document.getElementById('searchButton').addEventListener('click', function(event) {
     event.preventDefault();
     const movieTitle = document.getElementById('movieInput').value.trim();
+    
+    if (movieTitle) {
+        fetch('http://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=${apiKey}')
+            .then(response => response.json())
+            .then(data => {
