a800a2e21fc44142e67f579b22a64454a7675e2d
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-09-21 23:41:19 +0530

    project

---
+++ movie project/.js
@@ -0,0 +1,5 @@
+const apiKey = ' 9c8791f0'; // Replace with your API keyp
+
+document.getElementById('searchButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    const movieTitle = document.getElementById('movieInput').value.trim();
