1ec1e78122a64e851e1f125257f26aa24effa276
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-09-22 18:32:05 +0530

    project

--- movie project/.js
+++ movie project/.js
@@ -4,3 +4,9 @@ const apiKey = 'b67c9c19';
 // Open the database connection
 let db;
 const request = indexedDB.open('myDatabase', 1);
+request.onupgradeneeded = (event) => {
+    db = event.target.result;
+    const objectStore = db.createObjectStore('movies', { keyPath: 'title' });
+    objectStore.createIndex('title', 'title', { unique: true });
+  };
+  
