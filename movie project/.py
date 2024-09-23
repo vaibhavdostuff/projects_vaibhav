5bc4dfcf9a61b4e45a5135944ba6044d74f4bb88
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-09-23 23:48:37 +0530

    project

--- movie project/.py
+++ movie project/.py
@@ -4,3 +4,12 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+app = Flask(__name__)
+
+# Define the absolute path for the CSV file
+file_path = 'C:\Users\Vaibhav Negi\OneDrive\Desktop\web scrapingmovie'  # Update this path
+
+# Function to scrape movie data from IMDb
+def scrape_movie_data(movie_title):
+    search_url = f"https://www.imdb.com/find?q={movie_title.replace(' ', '+')}&s=tt&ttype=ft"
+    
