d542c4007e5b3d650e0702ba235e857727fd6b05
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-10-02 16:10:54 +0530

    project

--- rephrase project/app.py
+++ rephrase project/app.py
@@ -1,3 +1,9 @@
 import spacy
 import random
 
+# Load spacy model for English
+nlp = spacy.load("en_core_web_sm")
+
+def restructure_sentence(text):
+    """Restructure the sentence to sound natural, allowing extra words and grammatical corrections."""
+    doc = nlp(text)
