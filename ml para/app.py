990730904f16667dbc1be2b7d3b2eafca35bece0
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-10-26 18:43:35 +0530

    project

--- ml para/app.py
+++ ml para/app.py
@@ -10,3 +10,9 @@ model_name = 't5-base'
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
 
+def set_seed(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
