a28ed83f085733a50539c3bd622c9abf185effd8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-10-26 18:43:06 +0530

    project

--- ml para/app.py
+++ ml para/app.py
@@ -2,3 +2,11 @@ from flask import Flask, render_template, request
 import random
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+app = Flask(__name__)
+
+# Load model and tokenizer
+model_name = 't5-base'
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+
