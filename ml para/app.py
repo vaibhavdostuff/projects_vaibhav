03834745dc5f7913733c6bf320941b68da79cc5d
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-11-25 19:02:13 +0530

    project

--- ml para/app.py
+++ ml para/app.py
@@ -6,6 +6,7 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 app = Flask(__name__)
 
 # Load model and tokenizer
+
 model_name = 't5-base'
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
