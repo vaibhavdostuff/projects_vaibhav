23baa7c0191ed9644496e394b1a7f11b5a8807c0
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-12-24 14:57:26 +0530

    project

--- google trends ml/app.py
+++ google trends ml/app.py
@@ -22,3 +22,18 @@ app.secret_key = 'your_secret_key_here'  # Change this to a random secret key
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max-limit
 
+class DataAnalyzer:
+    def __init__(self):
+        self.data = None
+        self.data_source = None
+
+    def load_data(self, file_path):
+        file_extension = os.path.splitext(file_path)[1].lower()
+        if file_extension == '.csv':
+            self.data = pd.read_csv(file_path)
+        elif file_extension in ['.xls', '.xlsx']:
+            self.data = pd.read_excel(file_path)
+        else:
+            raise ValueError("Unsupported file format")
+        
+        self.data_source = os.path.basename(file_path) 
