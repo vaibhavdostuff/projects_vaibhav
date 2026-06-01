65fbe2c5dffb1820026ba8b6f59b67b3ef32c4ba
vaibhavdostuff <148843245+vaibhavdostuff@users.noreply.github.com>
2026-06-01 13:39:12 +0530

    new pro

--- career-intelligence-platform/backend/ml/train_model.py
+++ career-intelligence-platform/backend/ml/train_model.py
@@ -1,25 +1,56 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 import joblib
 
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-df = pd.read_csv('datasets/resume_dataset.csv')
+df = pd.read_csv(
+    'ml/datasets/resume_dataset.csv'
+)
 
 X = df['resume_text']
 y = df['role']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
 model = Pipeline([
-    ('tfidf', TfidfVectorizer()),
-    ('clf', RandomForestClassifier())
+    (
+        'tfidf',
+        TfidfVectorizer(
+            stop_words='english'
+        )
+    ),
+    (
+        'classifier',
+        RandomForestClassifier(
+            n_estimators=200,
+            random_state=42
+        )
+    )
 ])
 
 model.fit(X_train, y_train)
 
-joblib.dump(model, 'career_model.pkl')
+predictions = model.predict(X_test)
 
-print('Model Trained')
+accuracy = accuracy_score(
+    y_test,
+    predictions
+)
+
+print(f'Accuracy: {accuracy:.2f}')
+
+joblib.dump(
+    model,
+    'ml/career_model.pkl'
+)
+
+print('Model Saved Successfully')
